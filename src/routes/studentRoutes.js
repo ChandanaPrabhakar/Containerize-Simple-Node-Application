d46bc5d4cbb7ff5b298ec57ee0fceeb5e55364bc
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-03-12 21:11:51 +0530

    changed the router file by creating controllers

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,64 +1,17 @@
 import express from 'express';
-import Students from '../model/studentModel';
+import { createStudentData,
+    getStudentsData,
+    getStudentById,
+    updateStudentById,
+    deleteStudentById
+ } from '../controller/studentController';
 
 const router = express.Router();
 
-//Create a student data.
-
-router.post('/students', async (req, res) =>{
-    try{
-        const student = await Students.create(req.body);
-        res.status(200).json(student);
-    }catch(error){
-        res.status(500).json({error: error.message});
-    }
-});
-
-//Get all the student data.
-
-router.get('/', async (req, res) =>{
-    try{
-        const students =  await Students.find();
-        res.status(200).json(students);
-    }catch(error){
-        res.status(500).json({error: error.message});
-    }
-});
-
-//Get student by id.
-
-router.get('/:id', async(req, res) =>{
-    try{
-        const student = await Students.findById(req.params.id);
-        if (!student) return res.status(404).json({ message: 'User not found' });
-        res.status(200).json(student);
-    }catch(error){
-        res.status(500).json({error: error.message});
-    }
-});
-
-//Update student by id.
-
-router.put('/:id', async(req, res) =>{
-    try{
-        const student = await Students.findByIdAndUpdate(req.params, req.body, {new: true});
-        if(!student) return res.status(404).json({message: 'Student not found'});
-        res.status(200).json(student);
-    }catch(error){
-        res.status(500).json({error: error.message});
-    }
-});
-
-// Delete student data
-
-router.delete('/:id', async(req, res) =>{
-    try{
-        const student = await Students.findByIdAndDelete(req.params.id);
-        if(!student) return res.status(404).json({message: 'Student not found'});
-        res.status(204).send();
-    }catch(error){
-        res.status(500).json({error: error.message});
-    }
-});
+router.post('/students', createStudentData);
+router.get('/', getStudentsData);
+router.get('/:id', getStudentById);
+router.put('/:id', updateStudentById);
+router.delete('/:id', deleteStudentById);
 
 export default router;
